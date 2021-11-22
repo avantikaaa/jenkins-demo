83692e02a84c11be59b369ac85e30d45877ec70a
avantika-mudumbai <avantika.mudumbai@gmail.com>
2021-11-22 23:16:11 +0530

    initial commit

---
+++ build.py
@@ -0,0 +1,13 @@
+def addition(n1, n2):
+    return (n1 + n2)
+
+def subtraction(n1, n2):
+    return (n1 - n2)
+
+def multiplication(n1, n2):
+    return (n1 * n2)
+
+def division(n1, n2):
+    if(n2!=0):
+        return (n1 / n2)
+    return 0
---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from build import *
+
+class Test(unittest.TestCase):
+    def test_addition(self):
+        data = [5, 10]
+        expected = 15
+        self.assertEqual(addition(data[0], data[1]), expected)
+
+    def test_subtraction(self):
+        data = [5, 10]
+        expected = -5
+        self.assertEqual(subtraction(data[0], data[1]), expected)
+
+    def test_multiplication(self):
+        data = [5, 10]
+        expected = 50
+        self.assertEqual(multiplication(data[0], data[1]), expected)
+
+    def test_division(self):
+        data = [10, 2]
+        expected = 5
+        self.assertAlmostEqual(division(data[0], data[1]), expected)
+
+if __name__ == '__main__':
+    unittest.main()
